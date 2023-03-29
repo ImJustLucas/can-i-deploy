19e5d72d751844f9c0f5c00e805d65fa03c767d2
ImJustLucas <bellierlucas.pro@gmail.com>
2023-03-29 16:12:17 +0200

    🎉 first commit

---
+++ src/pages/date/[date].tsx
@@ -0,0 +1,75 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import dayjs from "dayjs";
+
+export default function Date() {
+  const router = useRouter();
+
+  const [date, setDate] = useState<string>(router.query.date as string);
+  const [response, setResponse] = useState<boolean | null>(null);
+
+  const askApi = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/canIDeploy?date=${date}`
+      );
+      const data = await response.json();
+      setResponse(data.validated);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    askApi();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <IndexContainer>
+      <TitleContainer>
+        Can i <span>deploy ?</span>
+      </TitleContainer>
+      <ResponseContainer>
+        <span className="response">{response ? "Yes" : "No"}</span>
+        <br />
+        <span>{dayjs(date).format("DD/MM/YYYY")}</span>
+      </ResponseContainer>
+    </IndexContainer>
+  );
+}
+
+const IndexContainer = styled.div`
+  background-color: #1b2021;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleContainer = styled.p`
+  color: #ff8500;
+  font-size: 3rem;
+  text-transform: uppercase;
+  font-family: "Merienda", cursive;
+  color: #f9f9f9;
+
+  span {
+    color: #ff8500;
+  }
+`;
+
+const ResponseContainer = styled.div`
+  color: #f9f9f9;
+  font-size: 3rem;
+  text-transform: uppercase;
+  font-family: "Merienda", cursive;
+  text-align: center;
+
+  span.response {
+    color: #ff8500;
+  }
+`;
---
+++ src/pages/index.tsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import router from "next/router";
+import styled from "styled-components";
+
+import { Select, Button } from "@chakra-ui/react";
+
+export default function Home() {
+  const [response, setResponse] = useState<boolean | null>(null);
+  const [date, setDate] = useState<string>("");
+
+  // get the seven next days
+  const getSevenNextDays = () => {
+    const days = [];
+    for (let i = 0; i < 7; i++) {
+      const date = new Date();
+      date.setDate(date.getDate() + i);
+      days.push(date);
+    }
+    return days;
+  };
+
+  const setDateLink = (e: any) => {
+    setDate(e.target.value);
+  };
+
+  const goToDatePage = () => {
+    if (date === "") return;
+    router.push(`/date/${date}`);
+  };
+
+  const askApi = async () => {
+    const date = new Date().toISOString().slice(0, 10);
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/canIDeploy?date=${date}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setResponse(data.validated);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <IndexContainer>
+      <TitleContainer>
+        Can i <span>deploy ?</span>
+      </TitleContainer>
+      <FormContainer>
+        <Button colorScheme="orange" onClick={askApi}>
+          Can i Deploy today ?
+        </Button>
+        <br />
+        {response !== null && (
+          <ResponseConatiner>
+            <span>{response ? "Yes" : "No"}</span>
+          </ResponseConatiner>
+        )}
+      </FormContainer>
+      <hr />
+      <FormContainer>
+        <Select onChange={setDateLink} placeholder="Select a date">
+          {getSevenNextDays().map((day, index) => (
+            <option key={index} value={day.toISOString().slice(0, 10)}>
+              {day.toISOString().slice(0, 10)}
+            </option>
+          ))}
+        </Select>
+        <br />
+        <Button colorScheme="orange" onClick={goToDatePage}>
+          Check
+        </Button>
+      </FormContainer>
+    </IndexContainer>
+  );
+}
+
+const IndexContainer = styled.div`
+  background-color: #1b2021;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleContainer = styled.p`
+  color: #ff8500;
+  font-size: 3rem;
+  text-transform: uppercase;
+  font-family: "Merienda", cursive;
+  color: #f9f9f9;
+
+  span {
+    color: #ff8500;
+  }
+`;
+
+const ResponseConatiner = styled.p`
+  color: #f9f9f9;
+`;
+
+const FormContainer = styled.div`
+  width: 40%;
+  margin: 2rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
